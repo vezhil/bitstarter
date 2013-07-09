91389f187200874e76cf148d2ff343f2388c5d49
Ezhil Viswanathan <vezhil@hotmail.com>
2013-07-09 18:20:35 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-  response.send(buf.toString(fs.readFileSync('index.html')));
+  response.send(buf.toString(fs.readFileSync('index.html')),"utf-8");
 });
 
 var port = process.env.PORT || 5000;
